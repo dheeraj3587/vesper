64cec5e3cc56c2f7e103b88bd6400403077f2833
Dheeraj Joshi <anikatyonzon111@gmail.com>
2025-06-24 10:21:47 +0530

    fixed compiling errors

--- src/CodeGen.cpp
+++ src/CodeGen.cpp
@@ -784,7 +784,7 @@ bool CodeGen::assembleToBinary(const std::string &asmFile, const std::string &ou
     }
 
     // Step 2: Link to executable (macOS specific)
-    std::string linkCmd = "ld -arch x86_64 -macos_version_min 10.14 -e _main -o " + outputBinary + " " + objFile;
+    std::string linkCmd = "ld -arch x86_64 -macos_version_min 10.14 -e _main -lSystem -o " + outputBinary + " " + objFile;
     std::cout << "Linking: " << linkCmd << std::endl;
 
     int linkResult = std::system(linkCmd.c_str());
@@ -793,7 +793,7 @@ bool CodeGen::assembleToBinary(const std::string &asmFile, const std::string &ou
         std::cerr << "Error: Linking failed. Trying alternative approach..." << std::endl;
 
         // Alternative linking approach for macOS
-        std::string altLinkCmd = "gcc -arch x86_64 -o " + outputBinary + " " + objFile + " -nostdlib -e _main";
+        std::string altLinkCmd = "ld -arch x86_64 -e _main -static -o " + outputBinary + " " + objFile;
         std::cout << "Trying: " << altLinkCmd << std::endl;
 
         linkResult = std::system(altLinkCmd.c_str());
